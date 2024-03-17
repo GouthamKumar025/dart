11cfe9585e193484aa2d73a7ac6306ee17e6b58d
Goutham Kumar S <113292942+GouthamKumar025@users.noreply.github.com>
2024-03-17 21:16:57 +0530

    Create var_final_const.dart

---
+++ var_final_const.dart
@@ -0,0 +1,15 @@
+void main() {
+  //variables
+
+  // var/final/const variableName = value;
+
+  var someValue = '10';
+
+  final someValue2 = DateTime.now();
+
+  const someValue3 = '10';
+
+  print(someValue);
+  print(someValue2);
+  print(someValue3);
+}
